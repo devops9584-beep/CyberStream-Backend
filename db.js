0d7eb6e1edc126beabc7263fa64c656f732dfd16
Pablo Olivera <pablo.olivera.sistemas@gmail.com>
2025-12-28 19:25:20 -0300

    Ajuste técnico: Conexión híbrida para CI/CD

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/* const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
@@ -10,6 +10,30 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) throw err;
+  console.log("Conectado a MySQL exitosamente");
+});
+
+module.exports = connection;
+
+*/
+
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  // Si existe DB_HOST en el sistema (GitHub), lo usa. Si no, usa 'localhost' (Tu PC).
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  // Si existe DB_PASSWORD (GitHub), lo usa. Si no, usa '' (Tu PC).
+  password: process.env.DB_PASSWORD || '', 
+  database: process.env.DB_NAME || 'cyber-stream_db_cs_main'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error("Error de conexión:", err.message);
+    return;
+  }
   console.log("Conectado a MySQL exitosamente");
 });
 
